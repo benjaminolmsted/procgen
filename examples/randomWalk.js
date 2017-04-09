765f850dc8befe3ee67b8fb6805d25fc07fc0d13
Benjamin Olmsted <b@3pxh.com>
2017-04-09 10:36:40 -0700

    I made two sampler functions: evenCircles and archimedesSpiral.

--- examples/randomWalk.js
+++ examples/randomWalk.js
@@ -11,13 +11,33 @@ function stepFunc() {
     return pg.geometry.toCircle(2 * Math.PI * Math.random())
 }
 
+function evenCircleStart(numPoints, radius){
+    return pg.geometry.circlePoints(numPoints, radius);
+}
+
+function archSpiralStart(a, b, n, pointsPerCycle){
+    var startPoints = Array(n);
+    var theta = 0;
+    for(var i = 0; i < n*pointsPerCycle; i++){
+        theta += 2*Math.PI/pointsPerCycle;
+        var r = a + b*theta;
+        var point = [0,0];
+        point[0] = pg.geometry.toCircle(theta)[0]*r;
+        point[1] = pg.geometry.toCircle(theta)[1]*r;
+        startPoints[i] = point;
+    }
+    return startPoints;
+}
+
 pg.setPalette(new pg.Palette({colorFunc: pg.Palette.randGray}))
 pg.setPalette(new pg.Palette({color: "$000"}))
 
-var params = {nPaths: 10, nSteps: 100, stepSize: 1/10, renderer: pg.renderers.line}
+var params = {nPaths: 512, nSteps: 32, stepSize: 1/64, renderer: pg.renderers.line}
+var startPoints = archSpiralStart(0, 1/32, 4, 128);//evenCircleStart(params.nPaths, .5);
+
 for (var i = 0; i < params.nPaths; i++) {
-    var L = new pg.PointList({list: pg.random.walk2d(params.nSteps, {stepScale: params.stepSize, stepFunc: stepFunc})})
-    var points = L.points
+    var L = new pg.PointList({list: pg.random.walk2d(params.nSteps, {stepScale: params.stepSize, start: startPoints[i], stepFunc: null})})
+    var points = L.points;
     points.forEach(function(p, i) {
         if (i+1 < points.length) {
             pg.renderPoints(
