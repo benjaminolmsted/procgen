fc62c68fed2ac288c0abebe9b4741160a8f68d09
Benjamin Olmsted <b@3pxh.com>
2017-04-09 12:26:06 -0700

    this time with the right things written

--- examples/randomWalk.js
+++ examples/randomWalk.js
@@ -15,36 +15,11 @@ function evenCircleStart(numPoints, radius){
     return pg.geometry.circlePoints(numPoints, radius);
 }
 
-/*archimedeanSpiralPointSet
-a => the rotational offset
-b => controls the tightness and parity of the spiral
-from: https://en.wikipedia.org/wiki/Archimedean_spiral
-"in polar coordinates(r, theta) 
-r = a + b*theta
-nCycles => number of cycles
-nPoints => number of sample points in the set
-##FUTURE## even, growth, random spacing of points option
-
-*/
-function archSpiralPointSet(a, b, nCycles, nPoints){
-    var startPoints = [];
-    var pathLength = nCycles * 2*Math.PI;
-    for(var i = 0; i < nPoints; i++){
-        var theta = i*pathLength/nPoints;
-        var r = a + b*theta;
-        var point = [0,0];
-        point[0] = pg.geometry.toCircle(theta)[0]*r;
-        point[1] = pg.geometry.toCircle(theta)[1]*r;
-        startPoints.push(point);
-    }
-    return startPoints;
-}
-
 pg.setPalette(new pg.Palette({colorFunc: pg.Palette.randGray}))
 pg.setPalette(new pg.Palette({color: "$000"}))
 
 var params = {nPaths: 512, nSteps: 32, stepSize: 1/64, renderer: pg.renderers.line}
-var startPoints = archSpiralPointSet(0, 1/32, 4, 128);//evenCircleStart(params.nPaths, .5);
+var startPoints = pg.geometry.archSpiralPointSet(0, 1/32, 4, 128);//evenCircleStart(params.nPaths, .5);
 
 for (var i = 0; i < params.nPaths; i++) {
     var L = new pg.PointList({list: pg.random.walk2d(params.nSteps, {stepScale: params.stepSize, start: startPoints[i], stepFunc: null})})
--- source/geometry/index.js
+++ source/geometry/index.js
@@ -8,6 +8,29 @@ export function circlePoints(n, r) {
     return Array(n).fill(1).map((_, i) => toCircle(2 * Math.PI * i/n, r));
 }
 
+/*archimedeanSpiralPointSet
+a => the rotational offset
+b => controls the tightness and parity of the spiral
+from: https://en.wikipedia.org/wiki/Archimedean_spiral
+"in polar coordinates(r, theta) 
+r = a + b*theta
+nCycles => number of cycles
+nPoints => number of sample points in the set
+##FUTURE## even, growth, random spacing of points option
+
+*/
+function archSpiralPointSet(a, b, nCycles, nPoints){
+    var startPoints = [];
+    var pathLength = nCycles * 2*Math.PI;
+    for(var i = 0; i < nPoints; i++){
+        var theta = i*pathLength/nPoints;
+        var r = a + b*theta;
+        var point = pg.geometry.toCircle(theta, r);
+        startPoints.push(point);
+    }
+    return startPoints;
+}
+
 export function degToRad(theta) {
     return theta * Math.PI / 180;
 }
